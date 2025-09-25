bb90273b37b1d5d2af8fa93513c4c4f343ddaec6
Marinos.Sake <protutor13@hotmail.com>
2025-09-26 00:16:21 +0300

    chore:add global constants for game configuration

---
+++ flappy_bird/settings.py
@@ -0,0 +1,14 @@
+WIDTH, HEIGHT = 600, 600 #screen size
+FPS = 60 #frames per second
+
+PIPE_GAP = 200 #Spacing between pipes
+PIPE_WIDTH = 60 # wide pipes
+PIPE_SPEED = 3 # pipes's speed per frame
+PIPE_SPAWN_EVERY = 1500  # how often (ms) a new pair of pipes appears
+
+GRAVITY = 0.5 # constant added to the vertical velocity each frame. The larger it is, the faster the player falls
+JUMP_STRENGTH = -9.5 # negative value for the 'jump' (Space press). The larger the negative value, the higher the player launches.
+
+BG_COLOR = (20, 20, 28)
+FG_COLOR = (230, 230, 230)
+PIPE_COLOR = (80, 200, 120)
